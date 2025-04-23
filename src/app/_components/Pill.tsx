c4b472327656e106501ef41d01279bb130bc5bb7
Liliana-MP <liliana.pitra@gmail.com>
2025-04-23 20:15:57 +0200

    refactor

--- src/app/_components/Pill.tsx
+++ src/app/_components/Pill.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const Pill = ({ title, sponsored }: Props) => {
   return (
-    <div className={sponsored ? styles.sponsoredPill : styles.container}>
+    <div className={sponsored ? styles.sponsoredContainer : styles.container}>
       <p className={styles.bla}>{title}</p>
     </div>
   );
