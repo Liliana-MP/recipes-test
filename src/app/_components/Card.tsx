f7d7b59a9a0ccfb3903bd89503d8afdc368dd56c
Liliana-MP <liliana.pitra@gmail.com>
2025-04-24 18:59:38 +0200

    added routing

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Image from "next/image";
 import styles from "./card.module.scss";
 import { Pill } from "./Pill";
+import Link from "next/link";
 
 interface Props {
   data: {
@@ -12,45 +14,48 @@ interface Props {
       url: string;
       name: string;
     };
+    url: string;
   };
 }
 
 export const Card = ({ data }: Props) => {
-  const { sponsored, name, profiles, type, image } = data;
+  const { sponsored, name, profiles, type, image, url } = data;
 
   const typeText = type === "article" ? "Artikel" : "Recept";
 
   return (
-    <div className={styles.container}>
-      {sponsored && (
-        <div className={styles.sponsoredPill}>
-          <Pill title="Sponsrat" sponsored />
-        </div>
-      )}
-
-      <div className={styles.typePill}>
-        <Pill title={typeText} />
-      </div>
-
-      <div className={styles.recipeContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src={`https://img.koket.se/square-medium/${image.url}`}
-            alt={image.name}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <h4 className={styles.title}>{name}</h4>
-
-        {profiles && (
-          <div className={styles.profiles}>
-            {profiles.map((profile) => (
-              <span key={profile.id}>{profile.name}</span>
-            ))}
+    <Link href={type + url} target="_blank">
+      <div className={styles.container}>
+        {sponsored && (
+          <div className={styles.sponsoredPill}>
+            <Pill title="Sponsrat" sponsored />
           </div>
         )}
+
+        <div className={styles.typePill}>
+          <Pill title={typeText} />
+        </div>
+
+        <div className={styles.recipeContainer}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={`https://img.koket.se/square-medium/${image.url}`}
+              alt={image.name}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+          <h4 className={styles.title}>{name}</h4>
+
+          {profiles && (
+            <div className={styles.profiles}>
+              {profiles.map((profile) => (
+                <span key={profile.id}>{profile.name}</span>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
